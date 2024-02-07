86c5e4a3ba60017dacfd67242d3f979fb0b59e34
Gustav-Stack <lucenagustavo404@gmail.com>
2024-02-07 17:27:28 -0300

    feat: auth creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HomePageController } from './home-page/home-page.controller';
 import { HomePageService } from './home-page/home-page.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [HomePageController],
   providers: [HomePageService],
 })
