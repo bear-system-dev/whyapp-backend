583d2aa466d4314b40340cc66566df29a305df73
LestterX <lgs.alves@hotmail.com>
2024-02-07 16:30:57 -0300

    feat: UserCreateDTO | Swagger Plugin

---
+++ src/user/createUser.dto.ts
@@ -0,0 +1,6 @@
+export interface UserCreateDTO {
+  nome: string;
+  email: string;
+  senha: string;
+  avatar: string;
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,10 @@
 import { Body, Controller, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
-
+import { UserCreateDTO } from './createUser.dto';
 @Controller('user')
 export class UserController {
-  @Post()
-  create(
-    @Body()
-    data: { nome: string; email: string; senha: string; avatar: string },
-    @Res() res: Response,
-  ) {
+  @Post('cadastrar')
+  create(@Body() data: UserCreateDTO, @Res() res: Response) {
     if (!data.nome || !data.email || !data.avatar)
       return res.status(400).json({
         message: 'Você deve fornecer NOME, EMAIL, SENHA e AVATAR',
