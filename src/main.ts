28252ea0401ff3fcf96c36e5a6e74bd67d3358b8
LestterX <lgs.alves@hotmail.com>
2024-02-07 12:43:25 -0300

    feat: base config swagger | v1.0.1

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Exemplo')
-    .setDescription('Testando O Swagger')
-    .setVersion('0.0.1')
-    .addTag('exemplo')
+    .setTitle('WhyApp - Backend')
+    .setDescription(
+      `Aqui você encontra todos os endpoints da aplicação. Qualquer dúvida, entre em contato com o gerenciador do sistema.`,
+    )
+    .setVersion('v1.0.1')
+    .setLicense(
+      `©2023-2024 Bear System | Todos os direitos reservados`,
+      `https://bearsystem.onrender.com`,
+    )
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('v1/docs/api', app, document);
