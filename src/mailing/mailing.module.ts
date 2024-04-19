7d07e18e5046385128c6e38dba1b6775431e20fb
LestterX <lgs.alves@hotmail.com>
2024-04-18 23:18:11 -0300

    feat: refator emails env var so it doesnt stop application

--- src/mailing/mailing.module.ts
+++ src/mailing/mailing.module.ts
@@ -2,25 +2,9 @@ import { Module } from '@nestjs/common';
 import { MailingService } from './mailer.service';
 import { MailerModule } from '@nestjs-modules/mailer';
 
-const PROFESSIONAL_EMAIL = process.env.PROFESSIONAL_EMAIL
-  ? process.env.PROFESSIONAL_EMAIL
-  : undefined;
-const PROFESSIONAL_EMAIL_PASSWORD = process.env.PROFESSIONAL_EMAIL_PASSWORD
-  ? process.env.PROFESSIONAL_EMAIL_PASSWORD
-  : undefined;
-const PROFESSIONAL_NAME = process.env.PROFESSIONAL_NAME
-  ? process.env.PROFESSIONAL_NAME
-  : undefined;
-
-if (!PROFESSIONAL_EMAIL) {
-  throw new Error('Variável de ambiente PROFESSIONAL_EMAIL não foi encontrada');
-} else if (!PROFESSIONAL_EMAIL_PASSWORD) {
-  throw new Error(
-    'Variável de ambiente PROFESSIONAL_EMAIL_PASSWORD não foi encontrada',
-  );
-} else if (!PROFESSIONAL_NAME) {
-  throw new Error('Variável de ambiente PROFESSIONAL_NAME não foi encontrada');
-}
+const PROFESSIONAL_EMAIL = process.env.PROFESSIONAL_EMAIL || '';
+const PROFESSIONAL_EMAIL_PASSWORD =
+  process.env.PROFESSIONAL_EMAIL_PASSWORD || '';
 
 @Module({
   imports: [
